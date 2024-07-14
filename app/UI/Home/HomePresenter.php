521c55133f1790d52e8068d92d447ab375a94876
Milan Felix Šulc <rkfelix@gmail.com>
2024-07-14 18:24:21 +0200

    Home: cleanup

--- app/UI/Home/HomePresenter.php
+++ app/UI/Home/HomePresenter.php
@@ -3,21 +3,8 @@
 namespace App\UI\Home;
 
 use App\UI\BasePresenter;
-use RuntimeException;
-use Tracy\Debugger;
-use Tracy\ILogger;
 
 class HomePresenter extends BasePresenter
 {
-
-	public function handleException(): void
-	{
-		throw new RuntimeException(sprintf('This is example error. Date is "%s"', date('d.m.Y HH:mm:ss')));
-	}
-
-	public function handleError(): void
-	{
-		Debugger::log(sprintf('This is example log. Date is "%s"', date('d.m.Y HH:mm:ss')), ILogger::ERROR);
-	}
 
 }
